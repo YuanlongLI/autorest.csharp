7e1cf1dfb7672c68a55501ab785441a14f73df07
Anne Loomis Thompson <annelo@microsoft.com>
2020-01-20 14:52:18 -0800

    not real changes

--- eng/Generate.ps1
+++ eng/Generate.ps1
@@ -43,56 +43,56 @@ $launchSettings = Join-Path $autorestPluginProject 'Properties' 'launchSettings.
 $configurationPath = Join-Path $testServerDirectory 'readme.tests.md'
 $testServerSwaggerPath = Join-Path $repoRoot 'node_modules' '@microsoft.azure' 'autorest.testserver' 'swagger'
 $testNames =
-    'additionalProperties',
+    #'additionalProperties',
     #'azure-composite-swagger',
     #'azure-parameter-grouping',
     #'azure-report',
     #'azure-resource',
     #'azure-resource-x',
     #'azure-special-properties',
-    'body-array',
-    'body-boolean',
+    #'body-array',
+    #'body-boolean',
     #'body-boolean.quirks',
-    'body-byte',
-    'body-complex',
-    'body-date',
-    'body-datetime',
-    'body-datetime-rfc1123',
-    'body-dictionary',
-    'body-duration',
-    'body-file',
+    #'body-byte',
+    #'body-complex',
+    #'body-date',
+    #'body-datetime',
+    #'body-datetime-rfc1123',
+    #'body-dictionary',
+    #'body-duration',
+    #'body-file',
     #'body-formdata',
     #'body-formdata-urlencoded',
-    'body-integer',
-    'body-number',
+    #'body-integer',
+    #'body-number',
     #'body-number.quirks',
-    'body-string',
+    #'body-string',
     #'body-string.quirks',
     #'complex-model',
     #'composite-swagger',
     #'composite-swagger.quirks',
-    'custom-baseUrl',
-    'custom-baseUrl-more-options',
-    'custom-baseUrl-paging',
-    'extensible-enums-swagger',
+    #'custom-baseUrl',
+    #'custom-baseUrl-more-options',
+    #'custom-baseUrl-paging',
+    #'extensible-enums-swagger',
     #'head',
-    'header',
+    #'header',
     #'head-exceptions',
     #'httpInfrastructure',
     #'httpInfrastructure.quirks',
     #'lro',
     #'model-flattening',
-    'paging',
+    #'paging',
     #'parameter-flattening',
     #'report',
     #'required-optional',
     #'storage',
     #'subscriptionId-apiVersion',
-    'url',
-    'validation',
-    'xml-service',
+    #'url',
+    #'validation',
+    #'xml-service',
     #'xms-error-responses',
-    'url-multi-collectionFormat';
+    #'url-multi-collectionFormat';
 
 foreach ($testName in $testNames)
 {
@@ -118,7 +118,7 @@ foreach ($file in Get-ChildItem $testSwaggerPath)
     }
 }
 # Sample configuration
-$projectNames = 'AppConfiguration', 'CognitiveServices.TextAnalytics'
+$projectNames = 'FormRecognizer'
 
 foreach ($projectName in $projectNames)
 {
---
+++ samples/FormRecognizer/readme.md
@@ -0,0 +1,11 @@
+# FormRecognizer
+### AutoRest Configuration
+> see https://aka.ms/autorest
+
+``` yaml
+title: FormRecognizer
+require: $(this-folder)/../readme.samples.md
+# https://github.com/Azure/azure-rest-api-specs/blob/master/specification/cognitiveservices/data-plane/FormRecognizer/preview/v2.0/FormRecognizer.json
+input-file: $(this-folder)/FormRecognizer-v2.0.json
+namespace: Azure.AI.FormRecognizer
+```
